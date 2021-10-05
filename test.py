044a53d4a000558e9040bfa6ca4d2b5f213e11b5
Carterfmeng <mf654428310@gmail.com>
2021-10-05 11:34:09 +0800

    finish lab07

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-def retur_a():
-    a = 1
-    return a
+class ReverseList():
 
-b = retur_a()
-print(b)
+    def __init__(self, item):
+        self.list = list(range(item))
+        print(self.list)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+
+        try:
+            return self.list.pop()
+        except:
+            raise StopIteration
