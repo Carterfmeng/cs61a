0bd999351d09d1f1268b0a02a7aeac7f305a9a2f
Carterfmeng <mf654428310@gmail.com>
2021-10-18 01:50:31 +0800

    disc06

---
+++ disc06.py
@@ -0,0 +1,29 @@
+class Link:
+    empty = ()
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+    def __getitem__(self, i):
+        if i == 0:
+            return self.first
+        return self.rest[i-1]
+    def __len__(self):
+        return 1 + len(self.rest)
+    def __repr__(self):
+        if self.rest is Link.empty:
+            return 'Link({})'.format(self.first)
+        else:
+            return 'Link({}, {})'.format(self.first, repr(self.rest))
+
+def flip_two(lnk):
+    """
+    >>> one_lnk = Link(1)
+    >>> flip_two(one_lnk)
+    >>> one_lnk
+    Link(1)
+    >>> lnk = Link(1, Link(2, Link(3, Link(4, Link(5)))))
+    >>> flip_two(lnk)
+    >>> lnk
+    Link(2, Link(1, Link(4, Link(3, Link(5)))))
+    """
