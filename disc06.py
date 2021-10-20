d5c8fe32cd66c832a00356044302f5a15313b27a
Carterfmeng <mf654428310@gmail.com>
2021-10-21 02:12:00 +0800

    finish disc06.py tutorial part

--- disc06.py
+++ disc06.py
@@ -27,12 +27,71 @@ def flip_two(lnk):
     >>> lnk
     Link(2, Link(1, Link(4, Link(3, Link(5)))))
     """
-    if lnk.rest is Link.empty:
-        return
-    else:
-        lnk.first, lnk.rest.first = lnk.rest.first, lnk.first
-        flip_two(lnk.rest.rest)
+    # if lnk.rest is Link.empty:
+    #     return
+    # else:
+    #     lnk.first, lnk.rest.first = lnk.rest.first, lnk.first
+    #     flip_two(lnk.rest.rest)
+    pos = lnk
+    while not (pos.rest is Link.empty):
+        pos.first, pos.rest.first = pos.rest.first, pos.first
+        pos = pos.rest.rest
 
 # def return_f(lnk):
 #     flip_two(lnk)
 #     return lnk
+
+def remove_duplicates(lnk):
+    """
+    >>> lnk = Link(1, Link(1, Link(1, Link(1, Link(5)))))
+    >>> unique = remove_duplicates(lnk)
+    >>> len(unique)
+    2
+    >>> len(lnk)
+    2
+    """
+    def helper(lnk):
+        if lnk.rest is Link.empty:
+            return
+        else:
+            if lnk.first == lnk.rest.first:
+                lnk.rest = lnk.rest.rest
+                remove_duplicates(lnk)
+            else:
+                remove_duplicates(lnk.rest)
+    helper(lnk)
+    return lnk
+
+def reverse(lnk):
+    """
+    >>> a = Link(1, Link(2, Link(3)))
+    >>> r = reverse(a)
+    >>> r.first
+    3
+    >>> r.rest.first
+    2
+    """
+    if lnk.rest is Link.empty:
+        return lnk
+    else:
+        rest_rev  = reverse(lnk.rest)
+        lnk.rest.rest = lnk
+        lnk.rest = Link.empty
+        return rest_rev
+    
+def max_product(lst):
+    """Return the maximum product that can be formed using lst
+    without using any consecutive numbers
+    >>> max_product([10,3,1,9,2]) # 10 * 9
+    90
+    >>> max_product([5,10,5,10,5]) # 5 * 5 * 5
+    125
+    >>> max_product([])
+    1
+    """
+    if lst == []:
+        return 1
+    else:
+        use_first = lst[0] * max_product(lst[2:])
+        not_use_fist = max_product(lst[1:])
+        return max(use_first, not_use_fist)
