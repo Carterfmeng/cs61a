b9c5567b078580f212479e2b687f4f175a6cd50a
carter <654428310@qq.com>
2022-01-21 15:57:13 +0800

    scheme problem 1-6

--- disc03.py
+++ disc03.py
@@ -4,16 +4,20 @@ def tree(label, branches=[]):
         assert is_tree(branch)
     return [label] + list(branches)
 
+
 # Selectors
 def label(tree):
     return tree[0]
 
+
 def branches(tree):
     return tree[1:]
+
 
 # For convenience
 def is_leaf(tree):
     return not branches(tree)
+
 
 def is_tree(tree):
     if type(tree) != list or len(tree) < 1:
@@ -23,8 +27,10 @@ def is_tree(tree):
             return False
     return True
 
+
 # Example tree construction
 t = tree(1, [tree(3, [tree(4), tree(5), tree(6)]), tree(2)])
+
 
 def tree_max(t):
     """Return the max of a tree."""
@@ -32,6 +38,7 @@ def tree_max(t):
         return label(t)
     else:
         return max([label(t)] + [tree_max(b) for b in branches(t)])
+
 
 def height(t):
     """Return the height of a tree"""
@@ -48,6 +55,7 @@ def square_tree(t):
     else:
         return tree(label(t) ** 2, [square_tree(b) for b in branches(t)])
 
+
 def find_path(tree, x):
     """
     >>> find_path(t, 5)
@@ -55,6 +63,7 @@ def find_path(tree, x):
     >>> find_path(t, 10) # returns None
     """
     path_now = []
+
     def helper(tree, x, path_now):
         if label(tree) == x:
             return path_now + [label(tree)]
@@ -63,12 +72,15 @@ def find_path(tree, x):
                 result_path = helper(b, x, path_now + [label(tree)])
                 if result_path != None:
                     return result_path
+
     return helper(tree, x, path_now)
+
 
 def prune(t, k):
     def helper(t, k, depth_now):
         if depth_now == k:
             return tree(label(t))
         else:
-            return tree(label(t), [ helper(b, k, depth_now + 1) for b in branches(t)])
+            return tree(label(t), [helper(b, k, depth_now + 1) for b in branches(t)])
+
     return helper(t, k, 0)
